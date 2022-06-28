cc7d3785f9582c5630755103381925e8eebf443a
Лера Гордеева <valeri.gorde@mail.ru>
2022-06-28 13:39:44 +0300

    Добавьте файлы проекта.

---
+++ Unit3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Unit3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Valeria";
+            Console.WriteLine(MyName);
+        }
+    }
+}
